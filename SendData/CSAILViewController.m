fa8ae97b68fd07d99ff6be16c92d654267b20a60
Grace C <glchriste@gmail.com>
2012-07-20 15:18:56 -0400

    Polishing comments.

--- SendData/CSAILViewController.m
+++ SendData/CSAILViewController.m
@@ -1,3 +1,16 @@
+//
+//  CSAILViewController.h
+//  SendData
+//
+//Copyright (c) 2012 Grace Christenbery
+//
+/*Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #import "CSAILViewController.h"
 #import "AVCamViewController.h"
 
@@ -11,7 +24,7 @@
     //The array storing connected devices.
 	dataPeers=[[NSMutableArray alloc] init];
 	
-	// Create the buttons
+	//Create the buttons
     UIButton *btnConnect = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 	[btnConnect addTarget:self action:@selector(connectToPeers:) forControlEvents:UIControlEventTouchUpInside];
 	[btnConnect setTitle:@"Seek iOS Cameras as Server" forState:UIControlStateNormal];
@@ -29,7 +42,7 @@
 	
 }
 
-// Connect to other peers in the area that are detected (uses BlueTooth).
+//Connect to other peers in the area that are detected (uses BlueTooth).
 - (void) connectToPeers:(id) sender{
     if([sender tag]==11)
     {
